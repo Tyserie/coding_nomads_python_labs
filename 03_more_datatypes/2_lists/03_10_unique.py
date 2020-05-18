48c5d1f49825649a94db7ef77eba36ce80f8a2e1
Tyserie <tomas.tyser@seznam.cz>
2020-05-18 21:14:53 +0200

    "trying finish unique"

--- 03_more_datatypes/2_lists/03_10_unique.py
+++ 03_more_datatypes/2_lists/03_10_unique.py
@@ -6,3 +6,18 @@ unique_list = [55, 'hi', 4, 13]
 
 
 '''
+list_ = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
+unique_list = []
+x = list_[0]
+
+for i in list_:
+    for y in list_[x:]:
+        if i == y:
+            unique_list = list.remove(i)
+            print(unique_list)
+            x += 1
+
+    else:
+        unique_list.append(i)
+        # x += 1
+print(unique_list)
