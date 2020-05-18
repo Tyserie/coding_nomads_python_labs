2abe1c63433d34dbad484197d7b5e556d2d96be2
Tyserie <tomas.tyser@seznam.cz>
2020-05-18 19:58:17 +0200

    "finishing flatten exercise"

--- 03_more_datatypes/2_lists/03_09_flatten.py
+++ 03_more_datatypes/2_lists/03_09_flatten.py
@@ -5,21 +5,33 @@ starting_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
 flattened_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 '''
-
+#Solution_1
 starting_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
-s = ""
-for i in starting_list:
-    for t in i:
-        s += str(t)
-        #x.extend(str(t))
+flattened_list = []
+a = starting_list[0]
+b = starting_list[1]
+c = starting_list[2]
+flattened_list.extend(a + b + c)
+print(flattened_list)
 
-list = []
-list.extend(s)
-print(list)
+#Solution_2
+flattened_list = []
+x = len(starting_list)
+while x != 0:
+    for i in range(x):
+        i = starting_list[0]
+        flattened_list.extend(i)
+        starting_list.pop(0)
+        x -= 1
+
+print(flattened_list)
 
 
 
 
 
-#print(str)
-#print(str(starting_list).strip('[]'))
+
+
+
+
+
