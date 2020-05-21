c77bfedc2647da29b6b3cef33b4e0290dcd23cc4
god <tomas.tyser@seznam.cz>
2020-05-21 06:37:48 -0700

    "updating some exercises"

--- 03_more_datatypes/2_lists/03_11_split.py
+++ 03_more_datatypes/2_lists/03_11_split.py
@@ -4,3 +4,20 @@ create a list of all the words in the string and print the word with the most
 occurrences.
 
 '''
+inpt_usr = input("Please enter a string: ")
+y = len(inpt_usr)
+x = inpt_usr.split()
+list0 = []
+
+#didnt read the task properly and created something else:(
+#while len(x) > 2:
+#    for i in x[1:y]:
+#        w = x[0]
+#        if len(w) >= len(i):
+#            x.remove(i)
+#        if len(w) < len(i):
+#            x.remove(w)
+#print(x)
+
+#correct solution
+
