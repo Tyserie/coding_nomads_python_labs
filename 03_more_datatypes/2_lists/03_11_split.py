f1806b7df3e088007cc5e6ea8af5b2ec367c4d43
Tyserie <tomas.tyser@seznam.cz>
2020-05-25 17:17:12 +0200

    small update

--- 03_more_datatypes/2_lists/03_11_split.py
+++ 03_more_datatypes/2_lists/03_11_split.py
@@ -4,12 +4,6 @@ create a list of all the words in the string and print the word with the most
 occurrences.
 
 '''
-'''
-Write a script that takes in a string from the user. Using the split() method,
-create a list of all the words in the string and print the word with the most
-occurrences.
-
-'''
 inpt_usr = input("Please enter a string: ")
 c = True
 g = 1
