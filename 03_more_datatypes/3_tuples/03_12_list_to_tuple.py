649e12fb8a69cee595341fd0848498ffc2ebf788
Tyserie <tomas.tyser@seznam.cz>
2020-05-25 18:26:32 +0200

    "list to tuple, tuple to list update"

--- 03_more_datatypes/3_tuples/03_12_list_to_tuple.py
+++ 03_more_datatypes/3_tuples/03_12_list_to_tuple.py
@@ -2,3 +2,10 @@
 Write a script that takes a list and turns it into a tuple.
 
 '''
+list_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+tupl_e = ()
+
+tupl_e = tuple(list_)
+print(tupl_e)
+
+
--- 03_more_datatypes/3_tuples/03_13_tuple_to_list.py
+++ 03_more_datatypes/3_tuples/03_13_tuple_to_list.py
@@ -2,3 +2,9 @@
 Write a script that takes a tuple and turns it into a list.
 
 '''
+tupl_e = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+list_ = []
+
+
+list_ = list(tupl_e)
+print(list_)
