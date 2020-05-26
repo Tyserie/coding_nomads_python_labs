eda936634b54642c6c84dd4c637ed99b331b19aa
Tyserie <tomas.tyser@seznam.cz>
2020-05-26 20:52:06 +0200

    "pairing_tuple done"

--- 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
+++ 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
@@ -11,23 +11,22 @@ Note: This lab might be challenging! Make sure to discuss it with your mentor
 or chat about it on our forum.
 
 '''
-lst_numbrs = [11, 23, 15, 7, 9, 1, 13, 5, 17, 19, 21, 3]
+lst_numbrs = [11, 23, 15, 7, 9, 1, 13, 5, 17, 21, 3, 68, 5, 13, 45, 69, 78]
 lst_numbrs.sort()
-l = len(lst_numbrs)
-print(lst_numbrs)
+odd_lngt = len(range(2,len(lst_numbrs)))
+if odd_lngt % 2 != 0:
+    lst_numbrs.append(0)
+l = int(len(lst_numbrs) / 2) +1
 x = int(0)
 y = int(2)
 list1 = []
 list2 = []
-
 for i in range(1,l):
     for e in lst_numbrs[x:y:1]:
         list1.append(e)
-    print()
     list2.append(tuple(list1))
-    print(list2)
     x += 2
     y += 2
     list1.clear()
-
-
+for i in list2:
+    print(i)
