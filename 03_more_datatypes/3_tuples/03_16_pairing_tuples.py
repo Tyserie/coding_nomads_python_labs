6a7f032540211928db1b65385bc39cc19d4282de
Tyserie <tomas.tyser@seznam.cz>
2020-05-26 15:19:45 +0200

    "updating some progress on pairing_tuples"

--- 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
+++ 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
@@ -11,3 +11,23 @@ Note: This lab might be challenging! Make sure to discuss it with your mentor
 or chat about it on our forum.
 
 '''
+lst_numbrs = [11, 23, 15, 7, 9, 1, 13, 5, 17, 19, 21, 3]
+lst_numbrs.sort()
+l = len(lst_numbrs)
+print(lst_numbrs)
+x = int(0)
+y = int(2)
+list1 = []
+list2 = []
+
+for i in range(1,l):
+    for e in lst_numbrs[x:y:1]:
+        list1.append(e)
+    print()
+    list2.append(tuple(list1))
+    print(list2)
+    x += 2
+    y += 2
+    list1.clear()
+
+
