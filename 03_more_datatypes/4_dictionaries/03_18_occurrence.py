4d0f994f59e76bac57cb2cc1f8b104ffb8ffada3
Tyserie <tomas.tyser@seznam.cz>
2020-05-27 10:58:12 +0200

    "occurrence update"

--- 03_more_datatypes/4_dictionaries/03_18_occurrence.py
+++ 03_more_datatypes/4_dictionaries/03_18_occurrence.py
@@ -6,3 +6,15 @@ user_input = "hello"
 result = {"h": 1, "e": 1, "l": 2, "o": 1}
 
 '''
+#usr_inp = input("Please enter a string: ")
+user_input = "hello"
+count = 1
+for i in user_input:
+    e = user_input[1]
+    chckd = []
+    if i not in chckd:
+        if i == e:
+            count += 1
+
+
+
