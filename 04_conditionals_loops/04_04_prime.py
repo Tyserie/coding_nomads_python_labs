e9c00db27697f45a78c21bff0b792863295e9096
Tyserie <tomas.tyser@seznam.cz>
2020-06-01 13:06:21 +0200

    "prime done"

--- 04_conditionals_loops/04_04_prime.py
+++ 04_conditionals_loops/04_04_prime.py
@@ -2,3 +2,11 @@
 Print out every prime number between 1 and 100.
 
 '''
+prime_nr = []
+for i in range(2, 100):
+    for e in range(2, i):
+        if (i % e) == 0:
+            break
+    else:
+        prime_nr.append(i)
+print(prime_nr)
