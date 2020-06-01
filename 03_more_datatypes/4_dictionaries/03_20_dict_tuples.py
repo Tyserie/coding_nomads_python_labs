b0811e402072944ba164aa12e03dae6a537516d1
Tyserie <tomas.tyser@seznam.cz>
2020-06-01 09:41:19 +0200

    "dict_tuples done"

--- 03_more_datatypes/4_dictionaries/03_20_dict_tuples.py
+++ 03_more_datatypes/4_dictionaries/03_20_dict_tuples.py
@@ -10,3 +10,11 @@ NOTE: Check out the Python docs and see whether you can come up with a solution,
       Feel free to discuss any questions you have with your mentor and on the forum!
 
 '''
+input_dict = {"item1": 5, "item2": 6, "item3": 1}
+result_list = []
+sort_orders = sorted(input_dict.items(), key=lambda x: x[1])
+
+for i in sort_orders:
+    x = i[0], i[1]
+    result_list.append(x)
+print(result_list)
