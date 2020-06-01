6f30f4faa915349a0ad206925ed90ee6b869d676
Tyserie <tomas.tyser@seznam.cz>
2020-06-01 10:37:01 +0200

    "divisible done"

--- 04_conditionals_loops/04_01_divisible.py
+++ 04_conditionals_loops/04_01_divisible.py
@@ -4,3 +4,16 @@ from the user and determines whether it is divisible by 3 using an if statement.
 Print the result.
 
 '''
+test = True
+while test == True:
+    print()
+    usr_inp = int(input("Please enter a number between 1 and 1,000,000,000: "))
+    if usr_inp > 1000000000:
+        print("Please choose only number between 1 and 1,000,000,000: ")
+        continue
+    if usr_inp % 3 == 0 and usr_inp < 1000000000:
+        print("The number is divisible by 3!")
+        test = False
+    else:
+        print("The number is not divisible by 3!")
+
