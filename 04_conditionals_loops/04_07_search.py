2fb83cf66f190ebc33d4acbe1f6164c880d4088b
Tyserie <tomas.tyser@seznam.cz>
2020-06-01 18:35:39 +0200

    "search done"

--- 04_conditionals_loops/04_07_search.py
+++ 04_conditionals_loops/04_07_search.py
@@ -4,3 +4,17 @@ Receive a number between 0 and 1,000,000,000 from the user.
 Use while loop to find the number - when the number is found exit the loop and print the number to the console.
 
 '''
+usr_inp = int(input("Please chose a number between 0 and 1,000,000,000: "))
+x = True
+result_ = None
+
+while x == True:
+    for num_br in range(0, 1000000000):
+        if num_br == usr_inp:
+            result_ = num_br
+            x = False
+            break
+print(result_)
+
+
+
