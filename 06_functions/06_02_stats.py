27d287a907f5d363a311ade31f6d973c762adbfd
Tyserie <tomas.tyser@seznam.cz>
2020-06-18 19:03:04 +0200

    "06_02_stats done"

--- 06_functions/06_02_stats.py
+++ 06_functions/06_02_stats.py
@@ -6,8 +6,13 @@ Print these values to the console when calling the function.
 
 example_list = [1, 2, 3, 4, 5, 6, 7]
 
-def stats():
+def stats(list):
   # define the function here
-  pass
+  list_min = min(list)
+  list_max = max(list)
+  list_sum = sum(list)
+  list_avr = list_sum / 7
+  print(list_min, list_max, list_sum, list_avr)
 
 # call the function below here
+stats(example_list)
