915365704b60258a147ac122990af2646a6ca6df
Tyserie <tomas.tyser@seznam.cz>
2020-06-18 18:41:07 +0200

    "06_01_tasks"

--- 06_functions/06_01_tasks.py
+++ 06_functions/06_01_tasks.py
@@ -5,11 +5,30 @@ Write a script that completes the following tasks.
 '''
 
 # define a function that determines whether the number is divisible by 4 or 7 and returns a boolean
+def num_div_by(number):
+    x = number % 4 == 0
+    y = number % 7 == 0
+    return print("Is", number, "divisible by 4?", x, "\nIs", number, "divisible by 7?", y)
+num_div_by(780)
 
 # define a function that determines whether a number is divisible by both 4 and 7 and returns a boolean
+def num_div_by(number):
+    x = number % 4 == 0 and number % 7 == 0
+    return print("Is", number, "divisible by 4 and 7?",  x)
+num_div_by(780)
 
 # take in a number from the user between 1 and 1,000,000,000
-
-# call your functions, passing in the user input as the arguments, and set their output equal to new variables 
-
+# call your functions, passing in the user input as the arguments, and set their output equal to new variables
 # print your new variables to display the results
+def num_div_by(number):
+    x = number % 4 == 0 and number % 7 == 0
+    return print("Is", number, "divisible by 4 and 7?",  x)
+
+usr_inp = int(input("Please enter number between 1 - 1 000 000 000: "))
+num_div_by(usr_inp)
+
+
+
+
+
+
