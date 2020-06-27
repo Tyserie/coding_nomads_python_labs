d51a939d72879ea9709be87d5cd02d27ec762d79
Tyserie <tomas.tyser@seznam.cz>
2020-06-27 14:41:49 +0200

    "06_03_three_functions done"

--- 06_functions/06_03_three_functions.py
+++ 06_functions/06_03_three_functions.py
@@ -3,7 +3,7 @@ Write a program with 3 functions. Each function must call
 at least one other function and use the return value to do something.
 
 '''
-#This program prints hostname, private and public IP of the server.
+#This program prints hostname and private and public IP of the server.
 
 # Importing libraries
 import socket
